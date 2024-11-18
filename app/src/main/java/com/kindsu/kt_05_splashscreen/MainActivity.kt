9de859c85086beabf03b3319515a8f61cb2a5293
Kindsu <KindsuCSM@users.noreply.github.com>
2024-11-18 19:06:19 +0100

    Initial commit

---
+++ app/src/main/java/com/kindsu/kt_05_splashscreen/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.kindsu.kt_05_splashscreen
+
+import android.graphics.drawable.AnimationDrawable
+import android.os.Bundle
+import android.view.View
+import android.view.ViewTreeObserver
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val screenSplash = installSplashScreen()
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        Thread.sleep(2000)
+        screenSplash.setKeepOnScreenCondition {false}
+    }
+
+}
